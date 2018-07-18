0c90ab2fde6dd762124466da78754f6091bd3982
jobo322 <jose.bolanos@correounivalle.edu.co>
2018-07-18 16:01:26 -0500

    extracted full metadata and create list of spectra data - work in progress

--- src/readXML.js
+++ src/readXML.js
@@ -1,9 +1,27 @@
 const fs = require('fs');
+const path = require('path');
 const XmlStream = require('xml-stream');
 
-var pathMetaboliteMetadata = '/home/abolanos/Downloads/';
-var pathNmrPeakList = '/home/abolanos/Downloads/';
-var pathNmrBrukerSpectra = '/home/abolanos/Downloads/';
+var pathMetaboliteMetadata = '/home/abolanos/hmdbProject/';
+var pathNmrPeakList = '/home/abolanos/hmdbProject/';
+var pathFidFiles = '/home/abolanos/hmdbProject/hmdb_fid_files/';
+
+//create a list of nmr files to import with the metadata
+var list = fs.readdirSync(pathFidFiles)
+var spectraData = {};
+list.forEach((e) => {
+    let temp = e.split('_');
+    let accession = temp[0];
+    let dimension = temp[1];
+    let code = temp[2];
+    if (!spectraData[accession]) spectraData[accession] = {};
+    let entry = spectraData[accession];
+    entry[code] = {
+        dimension: dimension === 'nmroned' ? 1 : 2,
+        fileName: e
+    }
+})
+
 
 var stream = fs.createReadStream(pathMetaboliteMetadata + 'urine_metabolites.xml');
 // var file = fs.createWriteStream('/home/abolanos/text');
@@ -12,11 +30,17 @@ var xmlStream = new XmlStream(stream);
 var fd = fs.openSync('metabolite.txt', 'a');
 var count = 0 //for debug
 xmlStream.on('endElement: metabolite', async (metabolite) => {
-    if (count > 0) return//for debug
+    // if (count > 0) return//for debug
+    let accession = metabolite['accession'].$text;
     let result = {};
     await parseChildren(metabolite.$children, result);
-    fs.write(fd, JSON.stringify(result) + ',', (err) => console.log('some thing was wrong with fs.write'));
-    fs.close(fd, (err) => console.log('aoeu'))//for debug
+    if (count === 0) {
+        prefix = '[';
+    } else {
+        prefix = ',';
+    }
+    fs.write(fd, prefix + JSON.stringify(result), handdleError);
+    // fs.close(fd, (err) => console.log(err))//for debug
     count++//for debug
 });
 
@@ -24,7 +48,7 @@ async function readNmrPeakList() {
 
 }
 
-async function parseChildren(children, result) {
+async function parseChildren(children, result) { // @TODO: review some problems with empty properties
     children.forEach((e, i, array) => {
         if (!e.$name) return;
         if (!Array.isArray(result)) {
@@ -45,5 +69,12 @@ async function parseChildren(children, result) {
     })
 }
 
+function handdleError(err) {
+    if (!err) return;
+    throw err
+}
 xmlStream.preserve('metabolite');
-// xmlStream.on('end', () => fs.close(fd)); //uncomment for final use
+xmlStream.on('end', () => {
+    fs.write(fd, ']', handdleError);
+    fs.close(fd, handdleError);
+}); //uncomment for final use
