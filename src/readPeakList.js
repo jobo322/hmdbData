6d085eb66b196833434d3d4e1c0abf18461e00d6
jobo322 <jose.bolanos@correounivalle.edu.co>
2018-08-12 15:55:29 -0500

    normalize 1d peaklist file with table header

--- src/readPeakList.js
+++ src/readPeakList.js
@@ -6,44 +6,41 @@ const papa = require('papaparse');
 var pathNmrPeakList = 'C:\\Users\\juanCBA\\Documents\\hmdbProject\\hmdb_nmr_peak_lists'
 
 fs.readdir(pathNmrPeakList, (err, listDir) => {
-    var resultJson = [];
+    var resultJson = {};
     listDir.forEach((file) => {
-        resultJson.push({});
-        var temp = resultJson[resultJson.length - 1];
+        if (file.toLowerCase().match('nmrtwod')) return
+        let splitFileName = file.split('_');
+        resultJson[splitFileName[0]] = {};
+        var temp = resultJson[splitFileName[0]];
         var peakListData = fs.readFileSync(path.join(pathNmrPeakList, file), 'utf8');
-        var original = String(peakListData)
-        peakListData = peakListData.replace(/\n\n/g, '\n');
+        var dataLowerCase = peakListData.toLowerCase();
+        var original = String(peakListData) // for debug
+        peakListData = peakListData.replace(/\n{2,}/g, '\n');
+
         if (peakListData.indexOf('\t') !== -1) {
-            peakListData = peakListData.replace(/\s*\t+\s*/g, ' ');
+            peakListData = peakListData.replace(/[ ]*\t+[ ]*/g, ';');
+        } else {
+            peakListData = peakListData.replace(/[ ]+/g, ' ');
         }
-        var result = peakListData.split('\n')
-        console.log(result.length)
-        if (result.length === 2) {
-            console.log('\n\nEL NOMBRE ES ' + file)
-            console.log(JSON.stringify(peakListData))
-            console.log('\n\n' + JSON.stringify(original))
+        peakListData = peakListData.replace(/[ ]+\.\.[ ]+/g, '-')
+        if (peakListData[peakListData.length - 1] === '\n') peakListData = peakListData.slice(0, peakListData.length - 1)
+
+        if (peakListData.toLowerCase().indexOf('table') !== -1) {
+            let result = peakListData.split('\n');
+            console.log(result.length)
+            if (result.length === 1) {
+                console.log('\n\nEL NOMBRE ES ' + file)
+                console.log(JSON.stringify(peakListData))
+                console.log('\n\n' + JSON.stringify(original))
+            }
+        } else {
+            // use some modification for file like  HMDB0000176_nmroned_1163_28507.txt
         }
         
-        // result.forEach((e, i, arr) => {
-        //     
-        //     let t = temporal.split('\n');
-        //     if (i%2 > 0) {
-        //         console.log(t)
-        //         // let headers = t[0].split(' ');
-        //         // console.log(headers);
-        //     } else {
-        //         let header = String(temporal);
-        //         temp[header] = []
-        //     }
-            
-        //     // arr[i] = temp;
-        // })
+        
+
+        //
+        
 
     })
 })
-
-// var result = papa.parse(peakListData, {
-//     header: true
-// })
-// console.log(result)
-// console.log(peakListData)
